adabb69cc687c8caf9d4aa848d92796df9dca655
kabi-r <programmerkabirr@gmail.com>
2023-05-21 17:04:24 +0600

    some

--- src/Component/News/News.jsx
+++ src/Component/News/News.jsx
@@ -10,7 +10,7 @@ const News = () => {
     <Card>
       <Card.Img variant="top" src={image_url} />
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
+        <Card.Title>Title: {title}</Card.Title>
         <Card.Text>
           {details}
         </Card.Text>
